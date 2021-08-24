ffe7d1479c40444a2ed79a0e037f6a9ed2c45824
prithviraj-maurya <mouryaprithviraj@gmail.com>
2021-08-24 13:37:16 +0530

    initial commit

---
+++ stream-lit-app-aqi-data.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('My First App')
+
+map_data = pd.DataFrame(
+    np.random.randn(1000,2) / [50,50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
+
+st.map(map_data)
