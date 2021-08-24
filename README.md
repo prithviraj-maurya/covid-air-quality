8f89916c322d620a056753a273f54a1bfb01934c
prithviraj-maurya <mouryaprithviraj@gmail.com>
2021-08-24 16:30:05 +0530

    created map to plot cities

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### This repo contains the anaylsis of Covid-19 pandemic lockdown and its effects on the environment and the society.
 
 This repository is mainly for the stramlit app for interactive visualization.
-Complete article can be found here: [https://midnight-parrot-28b.notion.site/Effect-on-Air-Quality-COVID-19-1aa99168d0e842129978ced0cbb22c83]
+
+Complete article can be found here: 
+https://midnight-parrot-28b.notion.site/Effect-on-Air-Quality-COVID-19-1aa99168d0e842129978ced0cbb22c83
--- stream-lit-app-aqi-data.py
+++ stream-lit-app-aqi-data.py
@@ -2,11 +2,35 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('My First App')
+st.title('Air Quality EDA')
 
-map_data = pd.DataFrame(
-    np.random.randn(1000,2) / [50,50] + [37.76, -122.4],
-    columns=['lat', 'lon']
-)
+## Read data
+data_path = './data/city_day.csv'
+cities_coord_path = './data/indian_cities_coord.csv'
 
-st.map(map_data)
+def load_data():
+    data = pd.read_csv(data_path)
+    cities_coord = pd.read_csv(cities_coord_path)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    cities_coord.rename(lowercase, axis='columns', inplace=True)
+    data['date'] = pd.to_datetime((data['date']))
+    data['year'] = data['date'].dt.year
+    data['month'] = data['date'].dt.month
+    data['day'] = data['date'].dt.day
+    # combine the coordinates of the cities
+    data = data.merge(cities_coord, on="city", how='inner')
+    return data
+
+# Load 1k rows
+data = load_data()
+# Notify the user
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data.head())
+
+## Map all the cities present in the dataset
+year = st.slider('Year', 2015, 2020, 2020)
+filtered_data = data[data['date'].dt.year == year]
+st.subheader(f'Map of all cities in the year {year}')
+st.map(filtered_data)
